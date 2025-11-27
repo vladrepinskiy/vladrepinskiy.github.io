9cec754dbe128b8d82a698f77549a90823c4459d
Vlad Repinsky <devrepinsky@gmail.com>
2025-11-27 19:35:50 +0100

    Replace Nav component with Sidebar; remove Contact page and implement SidebarThemeToggle for theme switching functionality.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,20 @@
 import { styled } from "goober";
 import { Route, Router } from "wouter";
-import { Nav } from "@/components/nav/Nav";
+import { Sidebar } from "@/components/sidebar/Sidebar";
 import { Home } from "@/components/pages/Home";
 import { CV } from "@/components/pages/CV";
 import { Lab } from "@/components/pages/Lab";
-import { Contact } from "../pages/Contact";
 import { About } from "../pages/About";
 
 export const App = () => {
   return (
     <Router>
       <AppContainer>
-        <Nav />
+        <Sidebar />
         <ContentArea>
           <Route path="/" component={Home} />
           <Route path="/cv" component={CV} />
           <Route path="/lab" component={Lab} />
-          <Route path="/contact" component={Contact} />
           <Route path="/about" component={About} />
         </ContentArea>
       </AppContainer>
